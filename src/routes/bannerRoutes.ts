309fda8e66506f0fa681e8aa857bc6dad3a6d180
Your Name <sasithrC>
2025-09-29 18:26:03 +0530

    fix file upload issue

--- src/routes/bannerRoutes.ts
+++ src/routes/bannerRoutes.ts
@@ -1,31 +1,42 @@
-import { Router } from 'express';
+
+import { Router, Response, NextFunction } from 'express';
+
+// Extend Request type to allow managementName
+import { Request } from 'express';
+interface BannerRequest extends Request {
+	managementName?: string;
+}
 import { BannerController } from '../controllers/bannerController';
 import { upload } from '../utils/fileUpload';
 
 const router = Router();
 const controller = new BannerController();
 
-// Get all banner management data
-router.get('/', (req, res) => controller.getAll(req, res));
+// Middleware to set managementName for banner
+const setBannerManagementName = (req: BannerRequest, res: Response, next: NextFunction) => {
+	req.managementName = 'banner';
+	next();
+};
 
-// Update banner management data with file upload for images
+router.get('/', (req, res) => controller.getAll(req, res));
 router.put(
 	'/',
-		upload.fields([
-			{ name: 'homepage.bannerOne.image1', maxCount: 1 },
-			{ name: 'homepage.bannerOne.image2', maxCount: 1 },
-			{ name: 'homepage.bannerOne.image3', maxCount: 1 },
-			{ name: 'homepage.bannerTwo.backgroundImage', maxCount: 1 },
-			{ name: 'aboutBanner.bannerOne.backgroundImage', maxCount: 1 },
-			{ name: 'aboutBanner.bannerTwo.backgroundImage', maxCount: 1 },
-			{ name: 'aboutBanner.bannerOne.images', maxCount: 4 },
-			{ name: 'aboutBanner.bannerTwo.images', maxCount: 4 }
-		 ]),
-		(req, res, next) => {
-			console.log('Incoming file fields:', Object.keys(req.files || {}));
-			next();
-		},
-		(req, res) => controller.update(req, res)
+	setBannerManagementName,
+	upload.fields([
+		{ name: 'homepage.bannerOne.imageOne', maxCount: 1 },
+		{ name: 'homepage.bannerOne.imageTwo', maxCount: 1 },
+		{ name: 'homepage.bannerOne.imageThree', maxCount: 1 },
+		{ name: 'homepage.bannerTwo.backgroundImage', maxCount: 1 },
+		{ name: 'aboutBanner.bannerOne.backgroundImage', maxCount: 1 },
+		{ name: 'aboutBanner.bannerTwo.backgroundImage', maxCount: 1 },
+		{ name: 'aboutBanner.bannerOne.images', maxCount: 4 },
+		{ name: 'aboutBanner.bannerTwo.images', maxCount: 4 }
+	]),
+	(req, res, next) => {
+		console.log('Incoming file fields:', Object.keys(req.files || {}));
+		next();
+	},
+	(req, res) => controller.update(req, res)
 );
 
 export default router;
